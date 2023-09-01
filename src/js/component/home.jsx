1b728981e5cb9dc462850c87abbf36160cd32a69
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-09-01 22:24:32 +0000

    first Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState} from "react";
+import ListItem from "./listItem";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [tasks, setTasks] = useState([]);
+	const [input, setInput] = useState('');
 
-//create your first component
-const Home = () => {
+	const addTask = () => {
+		if (input !== "")
+		{
+			setTasks([...tasks, input]);
+		}
+	};
+	const removeTask = (index) => {
+	const newTasks = [...tasks];
+	newTasks.splice(index, 1);
+	setTasks(newTasks);
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<h1>To-Do List</h1>
+		<div>
+			<input
+			type="text"
+			placeholder="Enter a task"
+			value={input}
+			onKeyDown={input => {
+                if (input.key === 'Enter') {
+					addTask();
+                }
+              }}
+			onChange={(e) => setInput(e.target.value)}
+			 />
 		</div>
+		<ul className="list-group">
+			{tasks.map((task, index) => (
+			<li className="list-group-item m-0" key={index}>{task}<button type="button" onClick={() => removeTask(index)} className="btn text-danger"><i class="fa-solid fa-x fa-lg"></i></button></li>
+		  ))}
+		</ul>
+		 </div>
 	);
 };
 
