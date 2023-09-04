6c0e2782b7598332c9570ba59881d091b199d827
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-09-04 12:21:09 +0000

    Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useState} from "react";
-import ListItem from "./listItem";
 
 function Home() {
 	const [tasks, setTasks] = useState([]);
@@ -18,10 +17,11 @@ function Home() {
 	setTasks(newTasks);
 	};
 	return (
-		<div>
-		<h1>To-Do List</h1>
-		<div>
-			<input
+		<div className="container-fluid text-center">
+		<p className="h1 display-1 text-secondary m-2">ToDos</p>
+		<div className="row justify-content-center">
+			<div className="col-6 m-3">
+			<input className="input-group"
 			type="text"
 			placeholder="Enter a task"
 			value={input}
@@ -31,13 +31,20 @@ function Home() {
                 }
               }}
 			onChange={(e) => setInput(e.target.value)}
+			maxlength="100"
 			 />
+			</div>
 		</div>
-		<ul className="list-group">
+
+		<div className="row justify-content-center">
+			<div className="col-8">
+			<ul className="list-group">
 			{tasks.map((task, index) => (
-			<li className="list-group-item m-0" key={index}>{task}<button type="button" onClick={() => removeTask(index)} className="btn text-danger"><i class="fa-solid fa-x fa-lg"></i></button></li>
+			<li className="list-group-item m-0 text-center d-inline-flex justify-content-between" key={index}>{task}<button type="button" onClick={() => removeTask(index)} className="btn text-danger" ><i className="fa-solid fa-x fa-lg "></i></button></li>
 		  ))}
 		</ul>
+			</div>
+		</div>
 		 </div>
 	);
 };
--- src/js/component/listItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useState} from "react";
-
-const  ListItem = (props) => {
-    return <li className="list-group-item">
-    {props.input}<i className="fa-solid fa-x fa-lg"></i>
-    </li>;
-}
-export default ListItem;
