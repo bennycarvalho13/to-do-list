7b88f3519599b57a9aee4a3243a85c31f896b000
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-09-04 18:21:59 +0000

    fixes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,32 +6,35 @@ function Home() {
 	const [input, setInput] = useState('');
 
 	const addTask = () => {
-		if (input !== "")
-		{
+		if (input === " " || input === "") {
+			alert("Task cannot be empty");
+		}else{
 			setTasks([...tasks, input]);
 		}
 	};
 	const removeTask = (index) => {
-	const newTasks = [...tasks];
-	newTasks.splice(index, 1);
-	setTasks(newTasks);
+		const tempArr = [...tasks];
+		tempArr.splice(index,1);
+		setTasks(tempArr);
+
 	};
 	return (
 		<div className="container-fluid text-center">
 		<p className="h1 display-1 text-secondary m-2">ToDos</p>
 		<div className="row justify-content-center">
 			<div className="col-6 m-3">
-			<input className="input-group"
+			<input className="input-group rounded"
 			type="text"
 			placeholder="Enter a task"
 			value={input}
-			onKeyDown={input => {
-                if (input.key === 'Enter') {
+			onKeyDown={event => {
+                if (event.key === 'Enter') {
 					addTask();
+					setInput("");
                 }
               }}
 			onChange={(e) => setInput(e.target.value)}
-			maxlength="100"
+			maxLength="100"
 			 />
 			</div>
 		</div>
@@ -39,9 +42,8 @@ function Home() {
 		<div className="row justify-content-center">
 			<div className="col-8">
 			<ul className="list-group">
-			{tasks.map((task, index) => (
-			<li className="list-group-item m-0 text-center d-inline-flex justify-content-between" key={index}>{task}<button type="button" onClick={() => removeTask(index)} className="btn text-danger" ><i className="fa-solid fa-x fa-lg "></i></button></li>
-		  ))}
+				{tasks.map((task, index) => (<li className="list-group-item m-0 d-inline-flex justify-content-between" key={index}>{task}<button type="button" onClick={() =>{removeTask(index)}} className="btn text-danger" ><i className="fa-solid fa-x fa-lg "></i></button></li>))}
+				<li className="list-group-item m-0 text-start text-secondary list-group-item-dark">{tasks.length === 1 ? tasks.length + " item on the list" : tasks.length + " items on the list"}</li>
 		</ul>
 			</div>
 		</div>
