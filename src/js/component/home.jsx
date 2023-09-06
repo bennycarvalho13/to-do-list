9c7efbd5b81c56d2a3d464fb804f6d517c67dd73
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-09-06 17:23:54 +0000

    animation

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,7 @@ function Home() {
 	const [input, setInput] = useState('');
 
 	const addTask = () => {
-		if (input === " " || input === "") {
+		if (input.trim() === "") {
 			alert("Task cannot be empty");
 		}else{
 			setTasks([...tasks, input]);
@@ -42,7 +42,7 @@ function Home() {
 		<div className="row justify-content-center">
 			<div className="col-8">
 			<ul className="list-group">
-				{tasks.map((task, index) => (<li className="list-group-item m-0 d-inline-flex justify-content-between" key={index}>{task}<button type="button" onClick={() =>{removeTask(index)}} className="btn" ><i className="fa-solid fa-x fa-lg "></i></button></li>))}
+				{tasks.map((task, index) => (<li className="list-group-item m-0 d-inline-flex justify-content-between" id="listItem" key={index}>{task}<button type="button" onClick={() =>{removeTask(index)}} className="btn" ><i className="fa-solid fa-x fa-lg "></i></button></li>))}
 				<li className="list-group-item m-0 text-start text-secondary list-group-item-dark">{tasks.length < 2 ? tasks.length < 1 ? "No Tasks in the list" : tasks.length + " Task in the list" : tasks.length + " Tasks in the list"}</li>
 		</ul>
 			</div>
